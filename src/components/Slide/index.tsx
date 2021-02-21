df57ae561070e563de48467a9115c8a2654c4c5a
Lil-C0der <howtousegmai1@gmail.com>
2021-02-21 20:19:56 +0800

    style: fix some typos

--- src/components/Slide/index.tsx
+++ src/components/Slide/index.tsx
@@ -3,11 +3,11 @@ import Slide, { ISlideProps } from './slide';
 import SlideItem, { ISlideItemProps } from './slideItem';
 
 // 交叉类型，除了 ISlideProps 还具有 Item 属性，用于导出
-export type ITabsComponent = FC<ISlideProps> & {
+export type ISlideComponent = FC<ISlideProps> & {
   Item: FC<ISlideItemProps>;
 };
 
-const TransSlide = Slide as ITabsComponent;
+const TransSlide = Slide as ISlideComponent;
 TransSlide.Item = SlideItem;
 
 export default TransSlide;

beccb7d6cf6794b59f01b9df04009cc8e9d43a4d
Lil-C0der <howtousegmai1@gmail.com>
2021-04-27 00:58:35 +0800

    feat: 新增 Braft-Editor 组件

--- src/views/Community/postDetail.tsx
+++ src/views/Community/postDetail.tsx
@@ -3,14 +3,37 @@ import { getPostDetail } from '@/network/post';
 import { parseMongoDate, parseTimeStamp } from '@/utils';
 import React, { FC, useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import 'braft-editor/dist/index.css';
+import BraftEditor, { ControlType } from 'braft-editor';
+import { Button } from 'woo-ui-react';
 
 interface IPostParams {
   postId: string;
 }
 
+const editorControls: ControlType[] = [
+  'undo',
+  {
+    key: 'redo', // 使用key来指定控件类型
+    title: '恢复' // 自定义控件title
+  },
+  'separator',
+  {
+    key: 'emoji',
+    title: 'emoji'
+    // text: 'emoji'
+  },
+  'separator',
+  {
+    key: 'clear',
+    text: '清空'
+  }
+];
+
 const PostDetail: FC = () => {
   const { postId } = useParams<IPostParams>();
   const [postDetail, setPostDetail] = useState<dataTypes.IPostDetail>();
+  const [editorState, setEditorState] = useState();
 
   const fetchPostDetail = useCallback(async () => {
     const { data } = await getPostDetail(postId);
@@ -21,6 +44,14 @@ const PostDetail: FC = () => {
   useEffect(() => {
     fetchPostDetail();
   }, [fetchPostDetail]);
+
+  const handleChange = (e: any) => {
+    console.log(e);
+  };
+
+  const onReply = () => {
+    console.log(editorState);
+  };
 
   return (
     <div className="post">
@@ -39,9 +70,10 @@ const PostDetail: FC = () => {
 
       <div className="post-replies-wrapper">
         {postDetail?.replies.length ? (
-          postDetail?.replies.map((reply) => (
-            <div className="post-reply">
-              <span className="reply-username">{reply.username}</span>
+          postDetail?.replies.map((reply, index) => (
+            <div className="post-reply" key={index}>
+              <span className="reply-index">{index + 1} 楼</span>
+              <span className="reply-creator">{reply.username}</span>
               <span className="reply-date">
                 {parseTimeStamp(reply.replyAt)}
               </span>
@@ -52,6 +84,22 @@ const PostDetail: FC = () => {
           <Placeholder />
         )}
       </div>
+
+      <BraftEditor
+        style={{ height: '400px' }}
+        className="post-reply-editor"
+        value={editorState}
+        controls={editorControls}
+        onChange={handleChange}
+      />
+      <Button
+        className="reply-btn"
+        size="lg"
+        btnType="primary"
+        onClick={onReply}
+      >
+        回复
+      </Button>
     </div>
   );
 };
